bd2c8048d81e954658d0c217088ea389d43c311e
PureUsagi <ngjunhancanoe@gmail.com>
2024-02-23 19:52:28 +0800

    Use lamdas

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -6,6 +6,8 @@ import se.edu.streamdemo.task.Task;
 
 import java.util.ArrayList;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Task (stream) manager");
@@ -15,10 +17,16 @@ public class Main {
         System.out.println("Printing all data ...");
         printAllData(tasksData);
 
-        System.out.println("Printing deadlines ...");
+        System.out.println("Printing deadlines ... (before sorting)");
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+
+        System.out.println("Printing dealines ... (after sorting)");
+        printDeadlinesUsingStream(tasksData);
+
+        ArrayList<Task> filteredList = filterByString(tasksData, "11");
+        printAllData(filteredList);
 
     }
 
@@ -44,6 +52,21 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .sorted((t1, t2) -> t1.getDescription().compareToIgnoreCase(t2.getDescription()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterByString(ArrayList<Task> tasks, String filterString) {
+        ArrayList<Task> filteredList = (ArrayList<Task>) tasks.stream()
+                .filter((t) -> t.getDescription().contains(filterString))
+                .collect(toList());
+
+        return filteredList;
     }
 
 }
